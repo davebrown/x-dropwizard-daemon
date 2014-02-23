94188b7390dadc8ff68df3016e611c2a846e0e29
Robert Boll <robertcboll@gmail.com>
2014-02-22 20:49:52 -0500

    Updating to use optional and Strings utility classes

--- src/main/java/com/robertcboll/dropwizard/daemon/DaemonApplication.java
+++ src/main/java/com/robertcboll/dropwizard/daemon/DaemonApplication.java
@@ -1,5 +1,8 @@
 package com.robertcboll.dropwizard.daemon;
 
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Optional;
+import com.google.common.base.Strings;
 import com.sun.akuma.Daemon;
 import io.dropwizard.Application;
 import io.dropwizard.Configuration;
@@ -7,50 +10,62 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.util.Properties;
 
 /**
- * Daemonizes the service, if the {@code PROPERTY_DAEMON} is set to {@code true}.
+ * Daemonizes the application, if the {@code PROPERTY_DAEMON} is set to {@code true}.
  */
 public abstract class DaemonApplication<T extends Configuration> extends Application<T> {
 
     public static final Logger log = LoggerFactory.getLogger(DaemonApplication.class);
 
-    protected final static String PROPERTY_DAEMON = "daemon";
+    @VisibleForTesting static final String PROPERTY_DAEMON = "daemon";
+    @VisibleForTesting static final String PROPERTY_PIDFILE = "pidfile";
+    @VisibleForTesting static final String DEFAULT_PIDFILE = "/var/run/daemon.pid";
 
-    protected static final String PROPERTY_PIDFILE = "pidfile";
-
+    /**
+     * Obtains the pidfile property, defaulting to {@code DEFAULT_PIDFILE} if not set. Delegates to
+     * {@code daemonize(pidfilePath)}.
+     *
+     * @return this DaemonApplication
+     * @throws Exception if daemonization fails
+     */
     protected DaemonApplication daemonize() throws Exception {
-        final String pidfile = System.getProperty(PROPERTY_PIDFILE);
-        if (pidfile != null && !"".equals(pidfile)) {
-            return daemonize(pidfile);
-        } else {
-            log.warn("Not daemonizing; missing {} system property.", PROPERTY_PIDFILE);
-            return this;
-        }
+        return daemonize(getPidfileOrDefault(System.getProperties()));
     }
 
+    /**
+     * Performs daemonization on the current process, if not done already. Writes pid to {@code pidfilePath} and
+     * sets up {@code deleteOnExit} on that file.
+     * @param pidfilePath the path of the file to hold the {@code pid}.
+     *
+     * @return this DaemonApplication
+     * @throws Exception if daemonization fails
+     */
     protected DaemonApplication daemonize(final String pidfilePath) throws Exception {
-        Daemon daemon = new Daemon();
-
+        final Daemon daemon = new Daemon();
         if (daemon.isDaemonized()) {
-            File pidfile = new File(pidfilePath);
-
+            final File pidfile = new File(pidfilePath);
             daemon.init(pidfile.getAbsolutePath());
 
             if (pidfile.exists()) {
                 pidfile.deleteOnExit();
-                log.debug("Setting {} to delete on exit.", pidfile.getAbsolutePath());
+            } else {
+                log.warn("Unable to set automatic deletion of pidfile {}.", pidfile.getAbsolutePath());
             }
-        } else {
-            if (shouldDaemonize()) {
-                daemon.daemonize();
-                System.exit(0);
-            }
+        } else if (shouldDaemonize(System.getProperties())) {
+            daemon.daemonize();
+            System.out.println("Launched daemon into background.");
+            System.exit(0);
         }
         return this;
     }
 
-    protected boolean shouldDaemonize() {
-        return Boolean.parseBoolean(System.getProperty(PROPERTY_DAEMON));
+    @VisibleForTesting String getPidfileOrDefault(final Properties props) {
+        return Optional.fromNullable(Strings.emptyToNull(props.getProperty(PROPERTY_PIDFILE))).or(DEFAULT_PIDFILE);
+    }
+
+    @VisibleForTesting boolean shouldDaemonize(final Properties props) {
+        return Boolean.parseBoolean(props.getProperty(PROPERTY_DAEMON));
     }
 }
